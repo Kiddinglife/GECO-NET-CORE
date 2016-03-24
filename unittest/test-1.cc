2c7b647478b1660d0e02eead2ef1de32dcb3e1c3
Kiddinglife <jakezhang1989@hotmail.com>
2016-03-25 10:36:25 +1100

    UP. from wins

--- unittest/test-1.cc
+++ unittest/test-1.cc
@@ -1,7 +1,13 @@
 #include "gtest/gtest.h"
-
+#include "geco-globals.h"
 TEST(testcase2, testHello2)
 {
-    EXPECT_EQ(true, true);
+    char result[32];
+    geco::ultils::Itoa(12, result, 10);
+    EXPECT_STREQ(result, "12");
+    geco::ultils::Itoa(12, result, 2);
+    EXPECT_STREQ(result, "1100");
+    geco::ultils::Itoa(12, result, 16);
+    EXPECT_STREQ(result, "c");
 }
 
